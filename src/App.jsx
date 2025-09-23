7b352b3ed0546477806ac032ae437216aa62f1a4
Rehman <malikabdulrehmanshabir@outlook.com>
2025-09-23 14:46:06 +0500

    'latest'

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import Routes from "./pages/Routes"
 import { useAuthContext } from './context/Auth'
 import { ConfigProvider } from 'antd'
+import ScreenLoader from './components/ScreenLoader.jsx'
 
 function App() {
   const { isAppLoading } = useAuthContext()
   return (
     <ConfigProvider theme={{ token: { colorPrimary: '#07887f' } }}>
-      {/* {isAppLoading
+      {isAppLoading
         ? <ScreenLoader />
         : <Routes />
-      } */}
-      <Routes />
+      }
     </ConfigProvider>
   )
 }

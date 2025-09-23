7ff1a7db8432c9f9634fd690612753d37ec5edd3
Rehman <malikabdulrehmanshabir@outlook.com>
2025-09-23 16:30:44 +0500

    'update'

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -12,8 +12,8 @@ const Index = () => {
   return (
     <Routes>
       <Route path='/*' element={<Frontend />} />
-      {/* <Route path='/auth/*' element={isAuth ? <Navigate to={user.role === "Donor" ? "/donor/donations" : "/dashboard/overview"} /> : <Auth />} /> */}
-      <Route path='/auth/*' element={!isAuth ? <Auth /> : <Navigate to="/" />} />
+      <Route path='/auth/*' element={isAuth ? <Navigate to={user.role === "Donor" ? "/donor/donations" : "/dashboard/overview"} /> : <Auth />} />
+      {/* <Route path='/auth/*' element={!isAuth ? <Auth /> : <Navigate to="/" />} /> */}
       <Route path='/donor/*' element={<PrivateRoute Component={DonorDashboard} role="Donor" />} />
       <Route path='/dashboard/*' element={<PrivateRoute Component={NgoDashboard} role="NGO" />} />
     </Routes>

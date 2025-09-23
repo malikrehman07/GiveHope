e4e05f37b6b51d082b3d09c74266dcccd93bcace
Rehman <malikabdulrehmanshabir@outlook.com>
2025-09-23 14:35:17 +0500

    'latest'

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -6,14 +6,14 @@ import { useAuthContext } from '../context/Auth'
 import PrivateRoute from '../components/PrivateRoute'
 import DonorDashboard from './DonorDashboard'
 import NgoDashboard from './NgoDashboard'
-import ProtectedAuth from '../components/ProtectedAuth'
 
 const Index = () => {
   const { user, isAuth } = useAuthContext()
   return (
     <Routes>
       <Route path='/*' element={<Frontend />} />
-      <Route path="/auth/*" element={<ProtectedAuth authOnly={true}><Auth /></ProtectedAuth>} />
+      {/* <Route path='/auth/*' element={isAuth ? <Navigate to={user.role === "Donor" ? "/donor/donations" : "/dashboard/overview"} /> : <Auth />} /> */}
+      <Route path='/auth/*' element={!isAuth ? <Auth /> : <Navigate to="/" />} />
       <Route path='/donor/*' element={<PrivateRoute Component={DonorDashboard} role="Donor" />} />
       <Route path='/dashboard/*' element={<PrivateRoute Component={NgoDashboard} role="NGO" />} />
     </Routes>

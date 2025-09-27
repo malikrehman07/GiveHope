e96536e6a222e4eaf171767877503d891e48f501
Rehman <malikabdulrehmanshabir@outlook.com>
2025-09-27 13:39:43 +0500

    'update'

--- src/pages/Frontend/Home/Companies/index.jsx
+++ src/pages/Frontend/Home/Companies/index.jsx
@@ -8,10 +8,10 @@ const Companies = () => {
             <div className='bg-primary'>
                 <div className="container py-3 ">
                     <Row gutter={16} className='text-center'>
-                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={2} className='text-white m-0'>TEAM<b>TALK</b></Title></Col>
-                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={2} className='text-white m-0'>EX<b>DONE</b> </Title></Col>
-                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={2} className='text-white m-0'><b>NEXT</b>FLOWS</Title></Col>
-                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={2} className='text-white m-0'>GLOBAL<b>CHART</b> </Title></Col>
+                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={3} className='text-white m-0'>TEAM<b>TALK</b></Title></Col>
+                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={3} className='text-white m-0'>EX<b>DONE</b> </Title></Col>
+                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={3} className='text-white m-0'><b>NEXT</b>FLOWS</Title></Col>
+                        <Col xs={12} sm={12} md={12} lg={6} className='my-3 text-center'><Title level={3} className='text-white m-0'>GLOBAL<b>CHART</b> </Title></Col>
                     </Row>
                 </div>
             </div>
